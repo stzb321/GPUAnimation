4266fbe64b8113d752e4b9229e2891c04af1d9cc
zhangzebin <zhangzebin@aobi.com>
2021-07-08 19:56:19 +0800

    添加animator controller editor

---
+++ Assets/Editor/GPUAnimatorControllerEditor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GPUAnimatiorController))]
+public class GPUAnimatorControllerEditor : Editor
+{
+    GPUAnimatiorController controller;
+    bool showAnimtions = true;
+
+    private void OnEnable()
+    {
+        controller = (GPUAnimatiorController)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.BeginVertical();
+
+        EditorGUILayout.Space();
+
+        showAnimtions = EditorGUILayout.Foldout(showAnimtions, "动画");
+        if(showAnimtions)
+        {
+            for (int i = 0; i < controller.animInfos.Length; i++)
+            {
+                var animInfo = controller.animInfos[i];
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(animInfo.animName);
+                if (GUILayout.Button("播放"))
+                {
+                    controller.Play(animInfo.animName);
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("speed");
+        float speed = EditorGUILayout.FloatField(controller.speed);
+        controller.speed = speed;
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndVertical();
+    }
+}
